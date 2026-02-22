50ca28fc72c7d2f28399c9710d7a0c7704585884
akashjagdale177 <akashjagdale177@gmail.com>
2026-02-22 09:00:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('<h1>Online Batch - Node js CICD</h1>')
+  res.send('<h1>Online Batch - Node js CICD</h1>
+           <h1> version = 1.0.0 </h1>')
 })
 
 app.listen(port,'0.0.0.0', () => {
